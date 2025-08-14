847515de8bfca9502a3901faed19f510c025743c
Andreas Foldvik Kemkers <akemkers@gmail.com>
2025-08-14 10:42:15 +0200

    change background on hyprlock + optimizations

--- packages/scripts/.config/scripts/check_fan_status.sh
+++ packages/scripts/.config/scripts/check_fan_status.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-
 CPU=$(sensors | awk '/cpu_fan/ {print $2}')
 GPU=$(sensors | awk '/gpu_fan/ {print $2}')
 
-echo "{\"cpu\": \"$CPU\", \"gpu\": \"$GPU\"}"
 echo "󰈐 CPU: $CPU RPM | GPU: $GPU RPM"
